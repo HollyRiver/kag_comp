82dbf61829c289dc0a484f3855387eb2326e2d6c
HollyRiver <hcssk2800@gmail.com>
2025-01-13 22:20:29 +0900

    .

--- Santa Metric/ga.py
+++ Santa Metric/ga.py
@@ -1,14 +1,13 @@
 import pandas as pd
-import numpy as np
 import pickle
 
 from algorithm import genetic
 
 df_sample = pd.read_csv("sample_submission.csv", index_col = 0)
-sample = df_sample.text.to_list()[4]
+sample = df_sample.text.to_list()[5]
 
-optimizr = genetic(sample, initial_times = 10000, cross_size = 100, cross_area = [12, 25, 50], max_stack = 5, crossover_method = "mixture", parent_size = 32, mutation_chances = 3, batch_size = 256, dupl = False)
+optimizr = genetic(sample, initial_times = 10000, cross_size = 80, cross_area = [25, 50, 100], max_stack = 3, crossover_method = "mixture", parent_size = 24, mutation_chances = 3, batch_size = 128, dupl = True)
 best_genome = optimizr.reputation(rep_times = 200)
 
-with open("sample4.pkl", "wb") as f :
+with open("sample5.pkl", "wb") as f :
     pickle.dump(best_genome, f)
